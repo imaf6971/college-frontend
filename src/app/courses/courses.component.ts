d6a0dbe0cb0ad11ee92c875d88f58926a912778f
Elvie Rayl <imaf6971@gmail.com>
2022-03-25 21:01:00 +0300

    container class now only applied on big screens

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { map, Observable, shareReplay } from 'rxjs';
 import { Course } from './interfaces/course';
 
 @Component({
@@ -8,8 +10,35 @@ import { Course } from './interfaces/course';
 })
 export class CoursesComponent  {
 
-  courses?: Course[];
+  courses?: Course[] = [
+    {
+      id: 1,
+      title: "Lol",
+      leader: "Lol1",
+    },
+    {
+      id: 1,
+      title: "Lol",
+      leader: "Lol1",
+    },
+    {
+      id: 1,
+      title: "Lol",
+      leader: "Lol1",
+    },
+    {
+      id: 1,
+      title: "Lol",
+      leader: "Lol1",
+    },
+  ];
 
-  constructor() { }
+  isNotHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => !result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
