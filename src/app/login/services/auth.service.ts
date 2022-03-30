404b935801e218e10bbde80c81375a6a5e3f9b0b
Elvie Rayl <imaf6971@gmail.com>
2022-03-30 18:20:36 +0300

    created stub for auth service

---
+++ src/app/login/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Credentials } from '../interfaces/credentials';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public isAuthenticated = new BehaviorSubject<boolean>(false);
+
+  constructor() {
+  }
+
+  login(user: Credentials) {
+    this.isAuthenticated.next(true)
+  }
+
+  logout() {
+    this.isAuthenticated.next(false);
+  }
+}
