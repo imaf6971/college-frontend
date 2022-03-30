f240490de94d5baf0de8a4949571ed9fd32b215c
Elvie Rayl <imaf6971@gmail.com>
2022-03-30 18:20:56 +0300

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,26 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: "courses", component: CoursesComponent}
+  {
+    path: 'courses',
+    component: CoursesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
 ];
 
 @NgModule({
