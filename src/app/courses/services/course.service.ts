59a667ca9919d326cb5fda616f59c49881a022d7
Elvie Rayl <imaf6971@gmail.com>
2022-03-31 21:19:06 +0300

    course service implemented

---
+++ src/app/courses/services/course.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Course } from '../interfaces/course';
+import { AbstractCourseService } from './course.service.abstract';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService implements AbstractCourseService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllCourses(): Observable<Course[]> {
+    return this.http
+      .get<Course[]>(`${environment.apiUrl}/api/v1/courses`);
+  }
+}
