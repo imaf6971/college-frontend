0fd75c490bc982f398b519a4ad1abf3c980b4293
Elvie Rayl <imaf6971@gmail.com>
2022-03-31 21:30:45 +0300

    added basic routing course detailed info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseInfoComponent } from './courses/course-info/course-info.component';
 import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
-  {path: "courses", component: CoursesComponent}
+  {
+    path: "courses",
+    component: CoursesComponent
+  },
+  {
+    path: 'courses/:id',
+    component: CourseInfoComponent
+  }
 ];
 
 @NgModule({
